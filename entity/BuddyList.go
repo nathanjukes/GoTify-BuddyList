0d5f545b7e6d02b38949afe18d664797737f1962
nathanjukes <njukes1@gmail.com>
2021-08-20 19:34:47 +0100

    Changed struct definition

--- entity/BuddyList.go
+++ entity/BuddyList.go
@@ -3,31 +3,37 @@ package entity
 import "time"
 
 type BuddyList struct {
-	Friends []struct {
-		Timestamp int64     `json:"timestamp"`
-		Time      time.Time `json:"time"`
-		User      struct {
-			URI      string `json:"uri"`
-			Name     string `json:"name"`
-			ImageURL string `json:"imageUrl"`
-		} `json:"user"`
-		Track struct {
-			URI      string `json:"uri"`
-			Name     string `json:"name"`
-			ImageURL string `json:"imageUrl"`
-			Album    struct {
-				URI  string `json:"uri"`
-				Name string `json:"name"`
-			} `json:"album"`
-			Artist struct {
-				URI  string `json:"uri"`
-				Name string `json:"name"`
-			} `json:"artist"`
-			Context struct {
-				URI   string `json:"uri"`
-				Name  string `json:"name"`
-				Index int    `json:"index"`
-			} `json:"context"`
-		} `json:"track"`
-	} `json:"friends"`
+	Friends []Friends `json:"friends"`
+}
+type User struct {
+	URI      string `json:"uri"`
+	Name     string `json:"name"`
+	ImageURL string `json:"imageUrl"`
+}
+type Album struct {
+	URI  string `json:"uri"`
+	Name string `json:"name"`
+}
+type Artist struct {
+	URI  string `json:"uri"`
+	Name string `json:"name"`
+}
+type Context struct {
+	URI   string `json:"uri"`
+	Name  string `json:"name"`
+	Index int    `json:"index"`
+}
+type Track struct {
+	URI      string  `json:"uri"`
+	Name     string  `json:"name"`
+	ImageURL string  `json:"imageUrl"`
+	Album    Album   `json:"album"`
+	Artist   Artist  `json:"artist"`
+	Context  Context `json:"context"`
+}
+type Friends struct {
+	Timestamp int64     `json:"timestamp"`
+	Time      time.Time `json:"time"`
+	User      User      `json:"user"`
+	Track     Track     `json:"track"`
 }
